c1b4f16313b3fbeeecb6c4166f9cd0e6eb89f6cf
Stefaan <safwebdev@gmail.com>
2019-09-29 00:05:05 +0800

    > datepicker on new education form

--- src/components/education/NewEducation.vue
+++ src/components/education/NewEducation.vue
@@ -22,12 +22,10 @@
                 </div>
                 <div class="row">
                     <div class="input-field col s6">
-                        <input type="text" v-model="edu_start" required>
-                        <label>Date Start</label>
+                        <datepicker placeholder="Start Date" type="text" v-model="edu_start" required />
                     </div>
                     <div class="input-field col s6">
-                        <input type="text" v-model="edu_end" required>
-                        <label>Date End</label>
+                        <datepicker placeholder="End Date" v-model="edu_end" type="text" required />
                     </div>
                 </div>
                 <div class="row">
@@ -44,7 +42,9 @@
 </template>
 
 <script>
-    import db from './../firebase/firebaseInit'
+    import db from './../firebase/firebaseInit';
+    import Datepicker from 'vuejs-datepicker';
+    import moment from 'moment';
     export default {
         name: 'new-education',
         
@@ -74,13 +74,19 @@
                     edu_id: this.edu_id,
                     edu_name: this.edu_name,
                     edu_field: this.edu_field,
-                    edu_start: this.edu_start,
-                    edu_end: this.edu_end,
+                    edu_start: moment(this.edu_start).format('YYYY-MM-DD'),
+                    edu_end: moment(this.edu_end).format('YYYY-MM-DD'),
                     edu_desc: this.edu_desc,
                 })
                 .then(docRef => this.$router.push('/'))
                 .catch(error => console.log(err))
+            },
+            customFormatter(date) {
+                return moment(date).format('YYYY-MM-DD');
             }
+        },
+        components: {
+            Datepicker
         }
     }
 </script>
