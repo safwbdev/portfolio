0d9fbc1b064d990cf499f47000f9577ebe45ba87
Stefaan <safwebdev@gmail.com>
2019-10-03 09:40:01 +0800

    environment variables remove config folder for commit

---
+++ src/components/firebase/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default{
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTHDOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASEURL,
+    projectId: process.env.FIREBASE_PROJECTID,
+    storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGINGDENDERID,
+    appId: process.env.FIREBASE_APPID
+}
